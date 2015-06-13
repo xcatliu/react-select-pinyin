f377a9060fa808437412b4b95433b0af8f702210
xcatliu <xcatliu@gmail.com>
2015-06-13 13:05:29 +0800

    Add Chinese hint

--- gh-pages/bundle.js
+++ gh-pages/bundle.js
@@ -20576,7 +20576,11 @@
 	            pyText = String(op.py);
 	        return !filterValue || this.props.matchPos === 'start' ? pinyinText.substr(0, filterValue.length) === filterValue || pyText.substr(0, filterValue.length) === filterValue || this.props.matchProp !== 'label' && valueText.toLowerCase().substr(0, filterValue.length) === filterValue || this.props.matchProp !== 'value' && labelText.toLowerCase().substr(0, filterValue.length) === filterValue : pinyinText.indexOf(filterValue.toLowerCase()) >= 0 || pyText.indexOf(filterValue.toLowerCase()) >= 0 || this.props.matchProp !== 'label' && valueText.toLowerCase().indexOf(filterValue.toLowerCase()) >= 0 || this.props.matchProp !== 'value' && labelText.toLowerCase().indexOf(filterValue.toLowerCase()) >= 0;
 	      },
-	      noResultsText: '找不到结果，请重新搜索'
+	      noResultsText: '找不到结果，请重新搜索',
+	      clearAllText: '清除所有已选',
+	      clearValueText: '清除已选',
+	      placeholder: '请选择...',
+	      searchPromptText: '请输入搜索'
 	    }, this.props);
 	    if (props.options.length === 0) {
 	      props.value = '';
--- src/SelectPinyin.jsx
+++ src/SelectPinyin.jsx
@@ -32,7 +32,11 @@ var SelectPinyin = React.createClass({
           )
         );
       },
-      noResultsText: '找不到结果，请重新搜索'
+      noResultsText: '找不到结果，请重新搜索',
+      clearAllText: '清除所有已选',
+      clearValueText: '清除已选',
+      placeholder: '请选择...',
+      searchPromptText: '请输入搜索'
     }, this.props);
     if (props.options.length === 0) {
       props.value = '';
